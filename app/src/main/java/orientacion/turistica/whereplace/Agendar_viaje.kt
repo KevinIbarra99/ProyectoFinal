d7462311b12042ff62da14966ed3925dad99f40b
ejim7 <enrique.jimenez181605@potros.itson.edu.mx>
2023-05-08 22:59:12 -0600

    Agendar Done
    :+1:

--- app/src/main/java/orientacion/turistica/whereplace/Agendar_viaje.kt
+++ app/src/main/java/orientacion/turistica/whereplace/Agendar_viaje.kt
@@ -4,21 +4,72 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Agendar_viaje : AppCompatActivity() {
+
+    private lateinit var storage: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agendar_viaje)
 
+        //Firebase
+        storage = FirebaseFirestore.getInstance()
 
-
+        //Botones
         val btn_regreso: ImageView = findViewById(R.id.regreso)
-        val btn_guardar_viaje: Button = findViewById(R.id.verviajes)
+        val btn_guardar_viaje: Button = findViewById(R.id.guardar)
         val btn_ver_proximos_viajes: Button = findViewById(R.id.recordar)
+
+        // Calendario
+        val calendar: CalendarView = findViewById(R.id.calendarView)
+
+        //Edit Text
+        val viaje: EditText = findViewById(R.id.multiAutoCompleteTextView)
+
+        //Extras
+        var nombre = intent.getStringExtra("nombre")
+        var correo = intent.getStringExtra("correo")
+
+        fun initCalendarView() {
+            calendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+                calendar.date = Calendar.getInstance()
+                    .apply { set(year, month, dayOfMonth) }
+                    .timeInMillis
+            }
+        }
+
+        btn_guardar_viaje.setOnClickListener {
+
+            initCalendarView()
+            storage.collection("viajes").document("${correo}").set(
+                hashMapOf(
+                    "lugar" to viaje.text.toString(),
+                    "fecha" to convertLongToTime(calendar.date)
+                )
+            ).addOnSuccessListener {
+                Toast.makeText(baseContext, "Viaje guardado", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(baseContext, "ERROR", Toast.LENGTH_SHORT).show()
+            }
+
+            val intent: Intent = Intent(this, Pantalla_Principal::class.java)
+            intent.putExtra("nombre", nombre)
+            intent.putExtra("correo", correo)
+            startActivity(intent)
+        }
 
         btn_regreso.setOnClickListener{
             val intent: Intent = Intent(this, Pantalla_Principal::class.java)
+            intent.putExtra("nombre", nombre)
+            intent.putExtra("correo", correo)
             startActivity(intent)
         }
         btn_ver_proximos_viajes.setOnClickListener{
@@ -26,4 +77,11 @@ class Agendar_viaje : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    fun convertLongToTime(time: Long): String {
+        val date = Date(time)
+        val format = SimpleDateFormat("yyyy/MM/dd")
+        return format.format(date)
+    }
+
 }
--- app/src/main/java/orientacion/turistica/whereplace/MenuProvisional.kt
+++ app/src/main/java/orientacion/turistica/whereplace/MenuProvisional.kt
@@ -17,24 +17,38 @@ class MenuProvisional : AppCompatActivity() {
         val btn_ajustes_configuracion: Button = findViewById(R.id.config)
         val btn_notificiones: Button = findViewById(R.id.notificacion)
 
+        var nombre = intent.getStringExtra("nombre")
+        var correo = intent.getStringExtra("correo")
+
+
         btn_regreso.setOnClickListener{
             val intent: Intent = Intent(this, Pantalla_Principal::class.java)
+            intent.putExtra("nombre", nombre)
+            intent.putExtra("correo", correo)
             startActivity(intent)
         }
         btn_perfil_usuario.setOnClickListener{
             val intent: Intent = Intent(this, Perfil_Usuario::class.java)
+            intent.putExtra("nombre", nombre)
+            intent.putExtra("correo", correo)
             startActivity(intent)
         }
         btn_favoritos.setOnClickListener{
             val intent: Intent = Intent(this, Favoritos::class.java)
+            intent.putExtra("nombre", nombre)
+            intent.putExtra("correo", correo)
             startActivity(intent)
         }
         btn_ajustes_configuracion.setOnClickListener{
             val intent: Intent = Intent(this, Ajustes::class.java)
+            intent.putExtra("nombre", nombre)
+            intent.putExtra("correo", correo)
             startActivity(intent)
         }
         btn_notificiones.setOnClickListener{
             val intent: Intent = Intent(this, Notificaciones::class.java)
+            intent.putExtra("nombre", nombre)
+            intent.putExtra("correo", correo)
             startActivity(intent)
         }
     }
